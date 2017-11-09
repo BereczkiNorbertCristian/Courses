da4c90776335bdd7ce1477facc63043a7433f12d
BereczkiNorbertCristian <bnorbertcristian@gmail.com>
2017-11-09 19:16:57 +0200

    Added lab 4 to ppd

--- ParallelAndDistributedProgramming/lab4/main.cc
+++ ParallelAndDistributedProgramming/lab4/main.cc
@@ -3,18 +3,23 @@
 #include<queue>
 #include<thread>
 #include<mutex>
-#include<conditional_variable>
+#include<condition_variable>
 
 using namespace std;
 
-vector<vector<int>> A,B,C,RES,INTER;
+vector<vector<int>> A,B,C,RES,INTER,INTER_ONE,RES_ONE;
 int n,m,k,t,workers;
-queue<int> complLines;
-conditional_variable cv;
+queue<pair<int,int>> complLines;
+condition_variable cv;
+mutex mtx;
+bool valueIsSet = false;
+
+void deb(string s){
+	cout << s << '\n';
+	cout.flush();
+}
 
 void initMatrix(vector<vector<int>>& X,int n,int m,int defVal=-1){
-
-	srand(time(NULL));
 
 	X.resize(n);
 	for(int i=0;i<n;++i){
@@ -28,34 +33,133 @@ void initMatrix(vector<vector<int>>& X,int n,int m,int defVal=-1){
 	}
 }
 
-void firstMul(int lineBeg,int lineEnd){
+void producer(int begIdx,int endIdx){
 
-	for(int i=lineBeg;i<lineEnd;++i){
-		for(int ki=0;ki<k;++ki){
-			for(int j=0;j<m;++j){
-				INTER[i][ki]+=(A[i][j]*B[j][ki]);
+	for(int i=begIdx;i<endIdx;++i){
+		for(int j=0;j<k;++j){
+			INTER[i][j] = 0;
+			for(int ii=0;ii<m;++ii){
+				INTER[i][j] += A[i][ii]*B[ii][j];
 			}
 		}
-		complLines.push(i);
 	}
+	unique_lock<mutex> lock(mtx);
+	complLines.push({begIdx,endIdx});
+	valueIsSet = true;
+	cv.notify_one();
+}
+
+void consumer(){
+
+	unique_lock<mutex> lock(mtx);
+	while(!valueIsSet)
+		cv.wait(lock);
+	pair<int,int> lines = complLines.front();
+	complLines.pop();
+
+	for(int line = lines.first;line < lines.second; ++line){
+		for(int i=0;i<t;++i){
+			RES[line][i] = 0;
+			for(int j=0;j<k;++j){
+				RES[line][i]+= INTER[line][j]*C[j][i];
+			}
+		}
+	}
+
+}
+
+void doOneThread(){
+
+	for(int i=0;i<n;++i){
+		for(int j=0;j<k;++j){
+			INTER_ONE[i][j] = 0;
+			for(int ki=0;ki<m;++ki){
+				INTER_ONE[i][j] += A[i][ki]*B[ki][j];
+			}
+		}
+	}
+
+	for(int i=0;i<n;++i){
+		for(int j=0;j<t;++j){
+			RES_ONE[i][j] = 0;
+			for(int ki=0;ki<k;++ki){
+				RES_ONE[i][j] += INTER_ONE[i][ki] * C[ki][j];
+			}
+		}
+	}
+
+}
+
+void printMatrix(vector<vector<int>>& MAT){
+
+	for(int i=0;i<n;i++){
+		for(int j=0;j<t;j++){
+			cout << MAT[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+}
+
+void doMoreThreads(){
+	thread producers[10];
+	thread consumers[10];
+	int step = n/workers;
+	int stepi = 0;
+	int noThrs=  0;
+	while(stepi < n){
+
+		int endStep = stepi + step;
+		if(endStep >= n){
+			endStep = n;
+		}
+		producers[noThrs] = thread(producer,stepi,endStep);
+		consumers[noThrs++] = thread(consumer);
+		stepi = endStep;
+	}
+
+	for(int i=0;i<noThrs;++i){
+		producers[i].join();
+		consumers[i].join();
+	}
+}
+
+template<typename F>
+void seeExecTime(F&& f){
+	auto t1 = std::chrono::high_resolution_clock::now();
+	f();
+	auto t2 = std::chrono::high_resolution_clock::now();
+	cout << "Duration nanos:" << '\n';
+	cout << std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count() << '\n';
 }
 
 int32_t main(){
 
 	freopen("input.in","r",stdin);
+	srand(time(NULL));
 
-	n = 2;
-	m = 2;
-	k = 2;
-	t = 2;
-	workers = 2;
+	n = 500;
+	m = 500;
+	k = 500;
+	t = 500;
+	workers = 10;
 
 	initMatrix(A,n,m,1);
 	initMatrix(B,m,k,2);
 	initMatrix(C,k,t,3);
 	initMatrix(RES,n,t,0);
 	initMatrix(INTER,n,k,0);
+	initMatrix(INTER_ONE,n,k,0);
+	initMatrix(RES_ONE,n,t,0);
 
+	cout << "One Thread" << '\n';
+	seeExecTime([](){doOneThread();});
+	cout << "More Threds" << '\n';
+	seeExecTime([](){doMoreThreads();});
+
+	if(n < 6 && t < 6){
+		printMatrix(RES);
+		printMatrix(RES_ONE);
+	}
 
 
 
